86aff7018625d34fd13c970e6397768f4c8ccddc
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-09-30 19:03:51 +0700

    Client Home Page

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+import "./assets/scss/main.scss";
+
+createApp(App)
+	.use(router)
+	.mount("#app");
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home container text-center">
+		<h1>Welcome to Txt<span>Bin</span>!</h1>
+		<h4>A Place to Paste Your Temporary Text</h4>
+		<div class="row justify-content-center">
+			<div class="col-12 col-md-8 col-lg-8">
+				<router-link class="btn btn-primary btn-block shadow-none btn-lg mt-4" to="/"
+					>Get Started</router-link
+				>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home"
+};
 </script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/mixins.scss";
+.home {
+	h1 {
+		font-weight: 700;
+	}
+	.btn {
+		@include mainButton();
+	}
+	padding: 30px;
+}
+</style>
